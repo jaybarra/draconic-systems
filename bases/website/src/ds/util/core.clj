68c54f9669a9138affc64fe6431c7d0ddf43c230
Jay Barra <jay.barra@draconic-systems.com>
2023-06-08 07:46:22 -0400

    removing unused test file

--- bases/website/src/ds/util/core.clj
+++ bases/website/src/ds/util/core.clj
@@ -1,7 +1,7 @@
 (ns ds.util.core
   (:require
    [camel-snake-kebab.core :as csk]
-   [clojure.string :as string]
+   [clojure.string :as str]
    [environ.core :refer [env]]))
 
 (defn get-env-value-or-file
@@ -11,7 +11,7 @@
         env-file (env (keyword (str (name env-var) "-file")))
         val (or (env env-var)
                 (when env-file
-                  (-> env-file slurp string/trim)))]
+                  (-> env-file slurp str/trim)))]
     (if val
       (assoc-in m path val)
       m)))
