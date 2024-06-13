9ffc9db913792e8a89bd576541f05dfe8dd94345
Jay Barra <jay.barra@draconic-systems.com>
2024-06-13 11:28:24 -0400

    Refactor form into reusable component
    Change the gist form into a reusable component

---
+++ lib/draconic_systems_web/live/gist_form_component.ex
@@ -0,0 +1,87 @@
+defmodule DraconicSystemsWeb.GistFormComponent do
+  use DraconicSystemsWeb, :live_component
+  alias DraconicSystems.{Gists, Gists.Gist}
+
+  def mount(socket) do
+    socket =
+      assign(
+        socket,
+        form: to_form(Gists.change_gist(%Gist{}))
+      )
+
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <div>
+      <.form for={@form} phx-submit="create" phx-change="validate" phx-target={@myself}>
+        <div class="justify-center px-28 w-full space-y-4 mb-10">
+          <!-- <.input type="hidden" field={@form[:id]} value={@id} /> -->
+          <.input
+            field={@form[:description]}
+            placeholder="Gist description..."
+            autocomplete="off"
+            phx-debounce="blur"
+          />
+          <div>
+            <div class="flex p-2 items-center bg-dsDark rounded-t-md border">
+              <div class="w-[300px] mb-2">
+                <.input
+                  field={@form[:name]}
+                  placeholder="File name including extension..."
+                  autocomplete="off"
+                  phx-debounce="blur"
+                />
+              </div>
+            </div>
+            <div id="gist-wrapper" class="flex w-full" phx-update="ignore">
+              <textarea id="line-numbers" class="line-numbers rounded-bl-md" readonly>
+          <%= "1\n" %>
+        </textarea>
+              <div class="flex-grow">
+                <.input
+                  id="gist-textarea"
+                  type="textarea"
+                  field={@form[:markup_text]}
+                  class="textarea w-full rounded-br-md"
+                  placeholder="Insert code..."
+                  spellcheck="false"
+                  autocomplete="off"
+                  phx-debounce="blur"
+                  phx-hook="UpdateLineNumbers"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="flex justify-end">
+            <.button class="create_button" phx-disable-with="Creating...">Create gist</.button>
+          </div>
+        </div>
+      </.form>
+    </div>
+    """
+  end
+
+  def handle_event("validate", %{"gist" => params}, socket) do
+    changeset =
+      %Gist{}
+      |> Gists.change_gist(params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, :form, to_form(changeset))}
+  end
+
+  def handle_event("create", %{"gist" => params}, socket) do
+    case Gists.create_gist(socket.assigns.current_user, params) do
+      {:ok, gist} ->
+        socket = push_event(socket, "clear-textarea", %{})
+        changeset = Gists.change_gist(%Gist{})
+        socket = assign(socket, :form, to_form(changeset))
+        {:noreply, push_navigate(socket, to: ~p"/gist?#{[id: gist]}")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, :form, to_form(changeset))}
+    end
+  end
+end
