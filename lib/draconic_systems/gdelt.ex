64c6caa0508e93f2b254967b2d531aeda6d3d847
Jay Barra <jay.barra@draconic-systems.com>
2024-06-14 19:12:25 -0400

    Alphabetize ordering of domains to exclude

--- lib/draconic_systems/gdelt.ex
+++
@@ -1,6 +0,0 @@
-defmodule DraconicSystems.Gdelt do
-  @master_csv_data_file_list_english "http://data.gdeltproject.org/gdeltv2/masterfilelist.txt"
-  @master_csv_data_file_list_translingual "http://data.gdeltproject.org/gdeltv2/masterfilelist-translation.txt"
-  @latest_csv_data_file_list_english "http://data.gdeltproject.org/gdeltv2/lastupdate.txt"
-  @latest_csv_data_file_list_translingual "http://data.gdeltproject.org/gdeltv2/lastupdate-translation.txt"
-end
